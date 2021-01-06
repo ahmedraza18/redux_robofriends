b047bc51b41e4562591cc285ce7528e687f3cd29
Ahmed Raza <artistahmed15@gmail.com>
2021-01-06 22:15:19 +0530

    Final commit to redux robofriends

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import "tachyons";
+
+import App from './containers/App';
+import reportWebVitals from "./reportWebVitals";
+import { searchRobots, requestRobots } from "./reducers";
+
+import "./index.css";
+
+const logger = createLogger()
+
+const rootReducers = combineReducers({ requestRobots, searchRobots })
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render( 
+    <React.StrictMode>
+    <Provider store = { store } >
+    <App / >
+    </Provider>   
+    </React.StrictMode > ,
+    document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
